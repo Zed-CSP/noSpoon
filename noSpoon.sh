0358a6f0e56c7f10ed0d4b5daeab8a57b787d260
Christopher Peret <cperet89@gmail.com>
2024-12-16 23:12:11 -0800

    working on falling spaces logic

--- noSpoon.sh
+++ noSpoon.sh
@@ -13,19 +13,26 @@ do
     sleep 0.05
 done | gawk '{
     a[$3]=0;                                                          # initialize array a with 0 for each column
-    for (x in a) {                                                    # for each column
-        o=a[x];                                                       # save the old value
-        a[x]=a[x]+1;                                                  # increment the value
-        printf "\033[%s;%sH\033[2;32m%s",o,x,$4;                      # print the old value
-        printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,$4;          # print the new value
-        if (a[x] >= $1) {                                             # if the value is greater than the number of rows
-            a[x]=0;                                                   # reset the value
-            printf "\033[%s;%sH\033[2;32m%s",a[x],x,$5;               # print a space
+    for (x in a) {                                                    # for each column in the array
+        o = a[x];                                                     # save the old value
+        a[x] = a[x]+1;                                                # increment the value
+        if ((a[x] <= $1) && (rand() < 0.075)) {                                        # if the value is less than the number of rows and a random number is less than 0.075 
+            printf "\033[%s;%sH\033[2;32m%s",o,x,$5;                      # print the old value as a space
+            printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,$5;          # print the new value as a space
+            if (a[x] >= $1) {                                             # if the value is greater than the number of rows
+                a[x]=0;                                                   # reset the value
+            } 
+            else {
+            printf "\033[%s;%sH\033[2;32m%s",o,x,$4;                      # print the old value 
+            printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,$4;          # print the new value
+            if (a[x] >= $1) {                                             # if the value is greater than the number of rows
+                a[x]=0;                                                   # reset the value
+            }
         }
     } 
 
 
-}}'
+}'
 
 #    if (rand() < 0.075) {
 #        col = int(rand() * $2) + 1;                    # random column
